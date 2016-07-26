b87f3c9b5d81599374336a7a54656c3ee72fa5a8
Bonnie Iker <bonniejiker@gmamil.com>
2016-07-26 15:08:45 -0400

    first commit, initial breakdown if/else statements

---
+++ guessing_game.rb
@@ -0,0 +1,28 @@
+
+
+def computer_number
+
+
+
+
+
+
+loop do
+
+
+
+
+rand 100
+Puts "Please guess a number between 1 and 100:"
+  input = gets.chomp
+
+
+  if input >= computer number
+    puts "Too low, please guess again"
+
+  elsif input <= random number
+    puts "Too high, please guess again"
+
+  else input == random number
+    puts "You WiN!"
+  break
