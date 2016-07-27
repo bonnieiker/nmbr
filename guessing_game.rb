78c8a9e0898f94aef85cca546e7a6b62889b9f1e
Bonnie Iker <bonniejiker@gmamil.com>
2016-07-26 22:25:43 -0400

    Making progress on making methods

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,20 +3,33 @@ number = rand(1...100)
 num_guesses = 0
 max_guesses = 5
 
-
+def check_guess(input, number)
+  if input == number
+    puts "You Win!"
+    break
+  elsif input < number
+    puts "Too low, guess again"
+  elsif input > number
+    puts "Too high, please guess again"
+  end
+end
 
 
 
 
 loop do
   puts "Please guess a number between 1 and 100:"
+
     input = gets.chomp.to_i
+    check_guess(input, number)
     num_guesses += 1
   if num_guesses == 5
     puts "YOU LOSE! Better luck next time"
+    puts "#{number}"
     break
   elsif input == number
       puts "YOU WiN!"
+    break
   elsif input < number
     puts "Too low, please guess again"
 
